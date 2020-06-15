85eff9a41e1cb25e94a83c5a2503f5a749d2466f
Ilya Shorin <shorin.ilia@yandex.ru>
2020-06-15 13:09:20 +0300

    доделать сайт, подключить javascript, написать скрипты

---
+++ scripts/script.js
@@ -0,0 +1,28 @@
+const editButton = document.querySelector(".profile__button_edit");
+const popup = document.querySelector(".popup")
+const closeButton = document.querySelector(".popup__button-close");
+
+function OpenPopup(){
+  popup.classList.add("popup_opened");
+}
+function ClosePopup(){
+  popup.classList.remove("popup_opened");
+}
+editButton.addEventListener("click", OpenPopup);
+
+const formElement = document.querySelector(".popup__container"); // Воспользуйтесь методом querySelector()
+let nameInput = document.querySelector(".popup__input-name"); // Воспользуйтесь инструментом .querySelector()
+let jobInput = document.querySelector(".popup__input-title"); // Воспользуйтесь инструментом .querySelector()
+let name = document.querySelector(".profile__name");
+let title = document.querySelector(".profile__title");
+nameInput.value = name.textContent;
+jobInput.value = title.textContent;
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    name.textContent = nameInput.value;
+    title.textContent = jobInput.value;
+    ClosePopup();
+}
+formElement.addEventListener('submit', formSubmitHandler);
+

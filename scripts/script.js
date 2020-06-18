101e98c41cf497daadefa6849cb5dc28dd4d199f
Ilya Shorin <shorin.ilia@yandex.ru>
2020-06-18 20:48:10 +0300

    исправить замечания ревьюера

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,26 @@
-const editButton = document.querySelector(".profile__button_edit");
+const editButton = document.querySelector(".profile__button-edit");
 const popup = document.querySelector(".popup");
 const closeButton = document.querySelector(".popup__button-close");
-
-
-editButton.addEventListener("click", OpenPopup);
-closeButton.addEventListener("click", ClosePopup);
-
 const formElement = document.querySelector(".popup__container"); // Воспользуйтесь методом querySelector()
-let nameInput = document.querySelector(".popup__input-name"); // Воспользуйтесь инструментом .querySelector()
-let jobInput = document.querySelector(".popup__input-title"); // Воспользуйтесь инструментом .querySelector()
+let nameInput = document.querySelector(".popup__input_name"); // Воспользуйтесь инструментом .querySelector()
+let jobInput = document.querySelector(".popup__input_title"); // Воспользуйтесь инструментом .querySelector()
 let name = document.querySelector(".profile__name");
 let title = document.querySelector(".profile__title");
 
-formElement.addEventListener("submit", formSubmitHandler);
-
-function formSubmitHandler(evt) {
-  evt.preventDefault();
-  name.textContent = nameInput.value;
-  title.textContent = jobInput.value;
-  popup.classList.remove("popup_opened");
-}
-function OpenPopup() {
+function openPopup() {
   nameInput.value = name.textContent;
   jobInput.value = title.textContent;
   popup.classList.add("popup_opened");
 }
-function ClosePopup() {
+function closePopup() {
   popup.classList.remove("popup_opened");
 }
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  name.textContent = nameInput.value;
+  title.textContent = jobInput.value;
+  closePopup();
+}
+editButton.addEventListener("click", openPopup);
+closeButton.addEventListener("click", closePopup);
+formElement.addEventListener("submit", formSubmitHandler);

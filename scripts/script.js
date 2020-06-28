0f229a42bf8252e482d062c4cccce92937417c91
Ilya Shorin <shorin.ilia@yandex.ru>
2020-06-28 23:28:00 +0300

    сделать функцию добавления / удаления карточек из массива. сделать лайк, сделать форму добавления карточки, сделать анимацию для форм, убрать outline

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,132 @@
-const editButton = document.querySelector(".profile__button-edit");
-const popup = document.querySelector(".popup");
-const closeButton = document.querySelector(".popup__button-close");
-const formElement = document.querySelector(".popup__container"); // Воспользуйтесь методом querySelector()
-let nameInput = document.querySelector(".popup__input_name"); // Воспользуйтесь инструментом .querySelector()
-let jobInput = document.querySelector(".popup__input_title"); // Воспользуйтесь инструментом .querySelector()
-let name = document.querySelector(".profile__name");
-let title = document.querySelector(".profile__title");
+///////////////////////// БЛОК ПЕРЕМЕННЫХ
 
+// изначальный массив карточек
+const initialCards = [
+  {
+    name: "Архыз",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+
+const editButton = document.querySelector(".profile__button-edit");
+const addButton = document.querySelector(".profile__button-add");
+const profilePopup = document.querySelector(".popup_profile");
+const cardsPopup = document.querySelector(".popup_cards");
+const closeButtonProfile = document.querySelector(
+  ".popup__button-close_profile"
+);
+const closeButtonCards = document.querySelector(".popup__button-close_cards");
+const profileFormElement = document.querySelector(".popup__container_profile");
+const cardFormElement = document.querySelector(".popup__container_card");
+const cardsList = document.querySelector(".cards");
+const cardsTemplate = document.querySelector(".cards-template");
+let popupInputProfileName = document.querySelector(
+  ".popup__input-profile-name"
+);
+let popupInputTitle = document.querySelector(".popup__input-title");
+let profileName = document.querySelector(".profile__name");
+let profileTitle = document.querySelector(".profile__title");
+let popupInputCardName = document.querySelector(".popup__input-card-name");
+let popupInputLink = document.querySelector(".popup__input-link");
+
+/////////////////////////
+
+/////////////////////////  БЛОК ФУНКЦИЙ
+
+//функция добавления карточки
+function addCards(name, link) {
+  const card = cardsTemplate.content.cloneNode(true);
+  card.querySelector(".cards__title").textContent = name;
+  card.querySelector(".cards__image").src = link;
+  card.querySelector(".cards__image").alt = name;
+  const likeButton = card.querySelector(".cards__button");
+  likeButton.addEventListener("click", function (evt) {
+    const eventTarget = evt.target;
+    eventTarget.classList.toggle("cards__button_active");
+  });
+  card.querySelector(".cards__delete-button").addEventListener("click",deleteCard);
+  cardsList.prepend(card);
+}
+
+//заполняем страницу из шаблона карточек
+initialCards.forEach((element) => {
+  addCards(element.name, element.link);
+});
+//функция удаления карточки
+function deleteCard(ev){
+  const card = ev.target.closest(".cards__item");
+  card.remove();
+}
+
+//функция открытия формы
 function openPopup() {
-  nameInput.value = name.textContent;
-  jobInput.value = title.textContent;
-  popup.classList.add("popup_opened");
+  function popup() {
+    profilePopup.classList.add("popup_opened");
+    popupInputProfileName.value = profileName.textContent;
+    popupInputTitle.value = profileTitle.textContent;
+  }
+  event.target.classList.value.includes("edit")
+    ? popup()
+    : cardsPopup.classList.add("popup_opened");
 }
+
+//функция закрытия формы
 function closePopup() {
-  popup.classList.remove("popup_opened");
+  let form = document.querySelector(".popup_opened");
+  form.classList.remove("popup_opened");
 }
+
+//функция отправки формы
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  name.textContent = nameInput.value;
-  title.textContent = jobInput.value;
+  function cardAction() {
+    let name = popupInputCardName.value;
+    let link = popupInputLink.value;
+    addCards(name, link);
+    popupInputCardName.value="";
+    popupInputLink.value="";
+  }
+  function profileAction() {
+    profileName.textContent = popupInputProfileName.value;
+    profileTitle.textContent = popupInputTitle.value;
+  }
+  evt.target.classList.value.includes("container_profile")
+    ? profileAction()
+    : cardAction();
   closePopup();
 }
+/////////////////////////
+
+///////////////////////// СЛУШАТЕЛИ КНОПОК
+profileFormElement.addEventListener("submit", formSubmitHandler);
+cardFormElement.addEventListener("submit", formSubmitHandler);
 editButton.addEventListener("click", openPopup);
-closeButton.addEventListener("click", closePopup);
-formElement.addEventListener("submit", formSubmitHandler);
+addButton.addEventListener("click", openPopup);
+closeButtonProfile.addEventListener("click", closePopup);
+closeButtonCards.addEventListener("click", closePopup);

d05a75c1059d0e756f4a70f7d84c26e94a73e4e9
Ilya Shorin <shorin.ilia@yandex.ru>
2020-07-16 17:29:27 +0300

    фикс кнопки

--- scripts/validate.js
+++ scripts/validate.js
@@ -38,11 +38,10 @@ function toggleButtonState(inputList, buttonElement) {
 function setEventListeners(formElement) {
   const inputList = Array.from(formElement.querySelectorAll(".popup__input"));
   const buttonElement = formElement.querySelector(".popup__button-save");
-  // toggleButtonState(inputList, buttonElement);
   inputList.forEach((inputElement) => {
     inputElement.addEventListener("input", function () {
       checkInputValidity(formElement, inputElement);
-      // toggleButtonState(inputList, buttonElement);
+      toggleButtonState(inputList, buttonElement);
     });
   });
 }
